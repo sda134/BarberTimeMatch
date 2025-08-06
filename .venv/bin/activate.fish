f46631ee52710174c70d514e61e6ced679cc6116
sda134 <saji.sus4@gmail.com>
2025-08-06 20:03:10 +0900

    fixed:userageng

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/sda/workspace/barbar_time_match/.venv
+set -gx VIRTUAL_ENV /home/sda/workspace/BarberTimeMatch/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
